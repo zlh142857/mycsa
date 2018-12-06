29b9b74e82c67b65b5d75f80188f7debd2b28b15
zlh142857 <1056846491@qq.com>
2018-12-06 17:08:50 +0800

    提交人:张立恒 提交日期:2010/12/06

--- eureka-client/src/main/java/com/hx/model/exam/LeakCase.java
+++ eureka-client/src/main/java/com/hx/model/exam/LeakCase.java
@@ -1,32 +1,28 @@
 package com.hx.model.exam;/*
  *//*
  *@作者:张立恒
- *@时间:2018/12/5 13:57
- *@功能:考试选择题表
+ *@时间:2018/12/6 16:58
+ *@功能:泄密案例表
  */
 
 import lombok.Data;
 
 import javax.persistence.*;
-import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Data
-public class ExamChoose implements Serializable {
-    private static final long serialVersionUID = 5767980566765986428L;
+public class LeakCase {
     @Id
     @Column(unique=true)
     @GeneratedValue(strategy=GenerationType.AUTO)//主键生成策略,默认情况下，JPA 自动选择一个最适合底层数据库的主键生成策略,MySQL 对应 auto increment
-    private Integer examId;//id;
-    private String examQuestion; //选择题题目
-    private String answera; //a选项内容
-    private String answerb;//a选项内容
-    private String answerc;//a选项内容
-    private String answer; //正确答案
+    private Integer id;//id;
+    private String caseTitle;
+    private String caseContent;
+    @Temporal( TemporalType.TIMESTAMP )
+    private Date caseTime;
     @Temporal( TemporalType.TIMESTAMP )
     private Date createTime;
     @Temporal( TemporalType.TIMESTAMP )
     private Date updateTime;
-
 }

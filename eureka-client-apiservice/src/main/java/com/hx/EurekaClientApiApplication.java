2b8abb16bfe75eb73a98ddf09f6337fd763279aa
zlh142857 <1056846491@qq.com>
2018-12-07 17:22:01 +0800

    提交人:张立恒 提交日期:2010/12/07

--- eureka-client-apiservice/src/main/java/com/hx/EurekaClientApiApplication.java
+++ eureka-client-apiservice/src/main/java/com/hx/EurekaClientApiApplication.java
@@ -1,11 +1,13 @@
 package com.hx;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableAutoConfiguration
 public class EurekaClientApiApplication {
 
     public static void main(String[] args) {
---
+++ eureka-client-apiservice/src/main/java/com/hx/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.hx.controller;/*
+ *//*
+ *@作者:张立恒
+ *@时间:2018/9/28 16:02
+ *@功能:
+ */
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+@RestController
+public class UserController {
+    @RequestMapping("test")
+    public void testSend1(){
+
+        // 创建动态客户端
+        JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
+        Client client = dcf.createClient("http://localhost:8088/services/UserService?wsdl");
+        // 需要密码的情况需要加上用户名和密码
+        // client.getOutInterceptors().add(new ClientLoginInterceptor(USER_NAME,PASS_WORD));
+        Object[] objects = new Object[0];
+        try {
+            // invoke("方法名",参数1,参数2,参数3....);
+            objects = client.invoke("getName", "111111");
+            System.out.println("返回数据:" + objects[0]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}

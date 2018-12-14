05394c380241c7f8bcc34c0b8da00a05ed801580
zlh142857 <1056846491@qq.com>
2018-12-14 11:01:37 +0800

    提交人:张立恒 提交日期:2018/12/14  工作流取消断路器降级操作

--- service-feign/src/main/java/com/hx/restController/ActivitiApproveController.java
+++ service-feign/src/main/java/com/hx/restController/ActivitiApproveController.java
@@ -27,14 +27,10 @@ public class ActivitiApproveController {
      * @date: 2018/12/10 13:42
      */
     @RequestMapping(value = "/lCountryRefer",method = RequestMethod.POST)
-    @HystrixCommand(fallbackMethod ="lCountryReferError")
     @ResponseBody
     public String lCountryRefer(@RequestParam("userInfo") UserInfo userInfo,@RequestParam("actMsgPersonnel") ActMsgPersonnel actMsgPersonnel){
         String referMsg=activitiApproveService.lCountryRefer(userInfo, actMsgPersonnel );
         return referMsg;
-    }
-    public String lCountryReferError(@RequestParam("userInfo") UserInfo userInfo,@RequestParam("actMsgPersonnel") ActMsgPersonnel actMsgPersonnel) {
-        return "服务未响应";
     }
     /**
      *
@@ -46,14 +42,10 @@ public class ActivitiApproveController {
      * @date: 2018/12/10 15:55
      */
     @RequestMapping(value = "/goWorkRefer",method = RequestMethod.POST)
-    @HystrixCommand(fallbackMethod ="goWorkReferError")
     @ResponseBody
     public String goWorkRefer(@RequestParam("userInfo") UserInfo userInfo,@RequestParam("actMsgPersonnel") ActMsgPersonnel actMsgPersonnel){
         String referMsg=activitiApproveService.goWorkRefer(userInfo, actMsgPersonnel );
         return referMsg;
-    }
-    public String goWorkReferError(@RequestParam("userInfo") UserInfo userInfo,@RequestParam("actMsgPersonnel") ActMsgPersonnel actMsgPersonnel) {
-        return "服务未响应";
     }
 
     /**
@@ -66,14 +58,10 @@ public class ActivitiApproveController {
      * @date: 2018/12/10 15:55
      */
     @RequestMapping(value = "/lPostRefer",method = RequestMethod.POST)
-    @HystrixCommand(fallbackMethod ="lPostReferError")
     @ResponseBody
     public String lPostRefer(@RequestParam("userInfo") UserInfo userInfo,@RequestParam("actMsgPersonnel") ActMsgPersonnel actMsgPersonnel){
         String referMsg=activitiApproveService.lPostRefer(userInfo, actMsgPersonnel );
         return referMsg;
-    }
-    public String lPostReferError(@RequestParam("userInfo") UserInfo userInfo,@RequestParam("actMsgPersonnel") ActMsgPersonnel actMsgPersonnel) {
-        return "服务未响应";
     }
     /**
      *
@@ -85,7 +73,6 @@ public class ActivitiApproveController {
      * @date: 2018/12/11 9:17
      */
     @RequestMapping(value = "/selectSelfLCTask",method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod ="selectSelfTaskError")
     @ResponseBody
     public String selectSelfLCTask(@RequestParam("uid") String uid){
         String map=activitiApproveService.selectSelfLCTask(uid);
@@ -101,7 +88,6 @@ public class ActivitiApproveController {
      * @date: 2018/12/11 10:06
      */
     @RequestMapping(value = "/selectSelfTWTask",method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod ="selectSelfTaskError")
     @ResponseBody
     public String selectSelfTWTask(@RequestParam("uid") String uid){
         String map=activitiApproveService.selectSelfTWTask(uid);
@@ -117,14 +103,10 @@ public class ActivitiApproveController {
      * @date: 2018/12/11 10:12
      */
     @RequestMapping(value = "/selectSelfLPTask",method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod ="selectSelfTaskError")
     @ResponseBody
     public String selectSelfLPTask(@RequestParam("uid") String uid){
         String map=activitiApproveService.selectSelfLPTask(uid);
         return map;
-    }
-    public String selectSelfTaskError(@RequestParam("uid") String uid) {
-        return "服务未响应";
     }
     /**
      *
@@ -136,7 +118,6 @@ public class ActivitiApproveController {
      * @date: 2018/12/11 13:30
      */
     @RequestMapping(value = "/aduitback",method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod ="aduitError")
     @ResponseBody
     public String aduitback(@RequestParam("taskId") String taskId){
         String msg=activitiApproveService.aduitback(taskId);
@@ -152,14 +133,10 @@ public class ActivitiApproveController {
      * @date: 2018/12/11 13:38
      */
     @RequestMapping(value = "/aduitok",method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod ="aduitokError")
     @ResponseBody
     public String aduitok(@RequestParam("taskId") String taskId,@RequestParam("uid")Integer uid){
         String msg=activitiApproveService.aduitok(taskId,uid);
         return msg;
-    }
-    public String aduitokError(@RequestParam("taskId") String taskId,@RequestParam("uid")Integer uid) {
-        return "服务未响应";
     }
     /**
      *
@@ -171,7 +148,6 @@ public class ActivitiApproveController {
      * @date: 2018/12/11 14:44
      */
     @RequestMapping(value = "/goComplete",method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod ="aduitError")
     @ResponseBody
     public String goComplete(@RequestParam("taskId") String taskId){
         String msg=activitiApproveService.goComplete(taskId);
@@ -187,14 +163,10 @@ public class ActivitiApproveController {
      * @date: 2018/12/11 15:39
      */
     @RequestMapping(value = "/applyAgain",method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod ="aduitError")
     @ResponseBody
     public String applyAgain(@RequestParam("taskId") String taskId){
         String msg=activitiApproveService.applyAgain(taskId);
         return msg;
-    }
-    public String aduitError(@RequestParam("taskId") String taskId) {
-        return "服务未响应";
     }
 
     /**
@@ -207,7 +179,6 @@ public class ActivitiApproveController {
      * @date: 2018/12/11 15:44
      */
     @RequestMapping(value = "/selectRunningTask",method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod ="selectTaskError")
     @ResponseBody
     public String selectRunningTask(@RequestParam("uid") Integer uid){
         String msg=activitiApproveService.selectRunningTask(uid);
@@ -223,13 +194,9 @@ public class ActivitiApproveController {
      * @date: 2018/12/11 16:14
      */
     @RequestMapping(value = "/selectRunedTask",method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod ="selectTaskError")
     @ResponseBody
     public String selectRunedTask(@RequestParam("uid") Integer uid){
         String msg=activitiApproveService.selectRunedTask(uid);
         return msg;
-    }
-    public String selectTaskError(@RequestParam("uid") Integer uid) {
-        return "服务未响应";
     }
 }

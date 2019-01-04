2c7b55da70378f125d062cbe450b1127f4c4c740
zlh142857 <1056846491@qq.com>
2019-01-04 09:47:37 +0800

    提交人:张立恒 提交日期:2018/01/04

--- eureka-client/src/test/java/com/hx/EurekaClientApplicationTests.java
+++ eureka-client/src/test/java/com/hx/EurekaClientApplicationTests.java
@@ -1,5 +1,4 @@
 package com.hx;
-import com.hx.config.utils.FileUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
--- service-feign/src/main/java/com/hx/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/hx/ServiceFeignApplication.java
@@ -4,6 +4,7 @@ import feign.Retryer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
